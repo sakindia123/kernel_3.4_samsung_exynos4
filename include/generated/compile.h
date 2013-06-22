9fa490977957e10c53fecd9821b9ff5fa9ed282c
sakindia123 <sa6915@gmail.com>
2013-06-22 16:33:23 +0530

    Initial commit with some samsung patches

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 0 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#0 PREEMPT Sat Jun 22 11:27:46 IST 2013"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "sarthak"
+#define LINUX_COMPILER "gcc version 4.7 (GCC) "
